4ceae27b09ed419e4b2ff7a0af3ceddac5235676
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-05-21 23:11:07 +0600

    google login imlemented

--- src/Pages/Auths/SocialLogin/SocialLogin.js
+++ src/Pages/Auths/SocialLogin/SocialLogin.js
@@ -7,9 +7,26 @@ import google from '../../../social/google.png';
 
 const SocialLogin = () => {
     const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
+    if (googleError) {
+        return (
+            <div>
+                <p>Error: {googleError.message}</p>
+            </div>
+        );
+    }
+
+
+    if (googleLoading) {
+        return <p>Loading...</p>;
+    }
+
+
     return (
         <div>
-            <button class="btn btn-outline border-gray-200 font-semibold w-full ml max-w-xl hover:text-white text-lg text-gray-800">
+            <button
+                onClick={() => signInWithGoogle()}
+                class="btn btn-outline  border-gray-200 font-semibold w-full ml max-w-xl hover:text-white text-lg text-gray-800">
                 <img style={{ width: '22px' }} src={google} className='mr-2' alt="" />
                 Google SIGn In
             </button>
