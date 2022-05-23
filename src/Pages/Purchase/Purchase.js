9b38be1332838fbe73f9c1159c9eec1617353293
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-05-23 12:59:50 +0600

    button disabled conditionaly

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 
 const Purchase = () => {
-    const countries = ["China", "Russia", "UK"];
-    const [menu, setMenu] = useState(false);
-    const [country, setCountry] = useState("United States");
+    const [disabled, setDisabled] = useState(false);
+    const [quantity, setQuantity] = useState();
 
-    const changeText = (e) => {
-        setMenu(false);
-        setCountry(e.target.textContent);
-    };
+    const orderQuantityChange = (event) => {
+        const inputQuantity = event.target.value;
+        setQuantity(inputQuantity);
+
+
+    }
+
     return (
         <div className="flex justify-center items-center">
             <div className="py-16 px-4 md:px-6 2xl:px-0 flex justify-center items-center 2xl:mx-auto 2xl:container">
@@ -27,7 +29,7 @@ const Purchase = () => {
                     </div>
 
                     <div className="flex flex-col xl:flex-row justify-center xl:justify-between space-y-6 xl:space-y-0 xl:space-x-6 w-full">
-                        <div className="xl:w-3/5 flex flex-col sm:flex-row xl:flex-col justify-center items-center bg-gray-100 py-7 sm:py-0 xl:py-10 px-10 xl:w-full">
+                        <div className=" flex flex-col sm:flex-row xl:flex-col justify-center items-center bg-gray-100 py-7 sm:py-0 xl:py-10 px-10 xl:w-full">
                             <div className="flex flex-col justify-start items-start w-full space-y-4">
                                 <p className="text-xl md:text-2xl leading-normal text-gray-800">Logitech K251</p>
                                 <p className="text-base font-semibold leading-none text-gray-600">$520.00</p>
@@ -41,8 +43,7 @@ const Purchase = () => {
                             <div>
                                 <h3 className='font-semibold'>About the product</h3>
                                 <p className=' mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit autem maxime sint inventore officiis vitae cum illo hic alias corporis?</p>
-                                <p className='font-semibold mb-3'>Available Quantity: 500</p>
-                                <p className='font-semibold mb-3'>Minimum Order: 100pcs</p>
+
                             </div>
                             <p className="text-base font-bold mt-5 leading-normal sm:leading-4 text-gray-600">
                                 Please provide necessary informations to purchase
@@ -61,18 +62,36 @@ const Purchase = () => {
                             </div>
 
 
-                            <label className="mt-8 text-base leading-4 text-gray-800">Order Quantity</label>
+
                             <div className="mt-2 flex-col">
+                                <p className='font-semibold mb-3 mt-6'>Available Quantity: 500</p>
+                                <p className='font-semibold mb-3'>Minimum Order: 100pcs</p>
                                 <div>
-                                    <input className="border rounded border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="number" placeholder="" min="100" max="500" />
+                                    <input
+                                        onChange={orderQuantityChange}
+                                        className="border rounded border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                                        type="number"
+                                        placeholder="Order Quantity"
+                                        min="100" max="500" />
+                                    {quantity < 100
+                                        &&
+                                        <p className="text-red-500 text-sm">Minimum order 100pcs</p>
+                                    }
+                                    {
+                                        quantity > 500
+                                        &&
+                                        <p className="text-red-500 text-sm">Maximum order 500pcs</p>
+                                    }
                                 </div>
                             </div>
 
 
 
-                            <button className="mt-8 border border-transparent hover:border-gray-300 bg-gray-900 hover:bg-white text-white hover:text-gray-900 flex justify-center items-center py-4 rounded w-full">
+                            <button
+                                disabled={(quantity < 100 || quantity > 500) && !disabled}
+                                className="mt-8 border border-transparent text-white btn btn-primary flex justify-center items-center py-4 rounded w-full">
                                 <div>
-                                    <p className="text-base leading-4">Pay $54652</p>
+                                    <p className="text-base leading-4">Place Order</p>
                                 </div>
                             </button>
                         </div>
