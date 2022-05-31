1bf508b52a657f7a2d5462d12b99fbe252d5319b
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-05-31 23:00:14 +0600

    navigated from 404 page

--- src/Pages/HomePage/Banner/Banner.js
+++ src/Pages/HomePage/Banner/Banner.js
@@ -1,21 +1,7 @@
 import React from "react";
-import banner3 from "../../../images/banner/banner-3.png";
-import banner4 from "../../../images/banner/banner-4.png";
-import banner5 from "../../../images/banner/banner-5.png";
 
 const Banner = () => {
   return (
-    // <div className="hero min-h-screen" style={{ backgroundImage: `url(${banner5})` }}>
-    //     <div className="hero-overlay bg-opacity-20" />
-    //     <div className="hero-content text-center text-slate-200">
-    //         <div className="max-w-md">
-    //             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-    //             <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    //             <button className="btn btn-primary">Get Started</button>
-    //         </div>
-    //     </div>
-    // </div>
-
     <div className="relative bg-primary">
       <div className="absolute inset-x-0 bottom-0">
         <svg
--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="bg-gray-800 h-screen">
+    <div className="h-screen">
       <div className="flex items-center justify-center py-12">
         <div className="bg-white border rounded-md flex items-center justify-center mx-4 md:w-2/3 ">
           <div className="flex flex-col items-center py-16 ">
@@ -22,8 +25,11 @@ const NotFound = () => {
             <p className="px-4 pb-10 text-base leading-none text-center text-gray-600">
               No signal here! we cannot find the page you are looking for{" "}
             </p>
-            <button className="mx-4 h-10 w-44 border rounded-md text-white text-base bg-indigo-700 hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-opacity-50 focus:ring-indigo-800">
-              Go Back
+            <button
+              onClick={() => navigate("/")}
+              className="mx-4 h-10 w-44 border rounded-md text-white text-base bg-indigo-700 hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-opacity-50 focus:ring-indigo-800"
+            >
+              Go To Home
             </button>
           </div>
         </div>
